832b0a35f7d312c4b6ed249af1c96e3af22fb9c2
Tyler Gordon <tagordon@uw.edu>
2017-09-07 23:29:35 -0700

    finished light curve simulator

--- light_curve_simulator.py
+++ light_curve_simulator.py
@@ -1,8 +1,15 @@
 import numpy as np
 import celerite
 from celerite import terms
+import batman
 
-def sim_lc(ppm, per_aper_ratio, periodic_freq, aperiodic_freq, A, t):
+def sim_lc(t, ppm, per_aper_ratio, periodic_freq, aperiodic_freq, A, planet=False, mean=1.0, planet_params=[0, 0, 0, 0, 0, 0, 0, 0, 0]):
+
+    if not planet:
+        mean=mean
+    if planet:
+        mean=transit_model(*planet_params, t)
+
     aperiodic_sigma = 1/per_aper_ratio
     periodic_sigma = 1
     white_noise_sigma = ppm/1e6
@@ -23,11 +30,18 @@ def sim_lc(ppm, per_aper_ratio, periodic_freq, aperiodic_freq, A, t):
     kernel += terms.SHOTerm(log_S0=np.log(S0), log_Q=np.log(Q), log_omega0=np.log(w0), bounds=bounds)
 
     # white noise
-    kernel += terms.JitterTerm(log_sigma=np.log(white_noise_sigma), bounds=dict(log_sigma=(-15,15)))
+    # kernel += terms.JitterTerm(log_sigma=np.log(white_noise_sigma), bounds=dict(log_sigma=(-15,15)))
 
     gp = celerite.GP(kernel, mean=0, fit_mean=True, fit_white_noise=True)
-    gp.compute(t, noise)
+    gp.compute(t, white_noise_sigma)
 
-    return A*gp.sample()+1
+    return A * gp.sample() + mean + white_noise_sigma * np.random.randn(len(t))
 
-    
+def transit_model(t0, per, ppm, a, inc, ecc, w, u0, u1, t):
+    rp = np.sqrt(ppm/1e6)
+    p = batman.TransitParams()
+    p.t0, p.per, p.rp, p.a, p.inc, p.ecc, p.w, u0, u1 = t0, per, rp, a, inc, ecc, w, u0, u1
+    p.u = [u0, u1]
+    p.limb_dark = "quadratic"
+    planet = batman.TransitModel(p, t)
+    return planet.light_curve(p)
